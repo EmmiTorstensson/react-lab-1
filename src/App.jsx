0062744632380cd95dc121e8c36309d136bd4fd9
”Emmi <emmitorstensson@hotmail.com>
2024-11-10 18:27:03 +0100

    added html

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+		<div className='hero'>
+			<h1>Partycentralen</h1>
+			<p>Skapa en unik fest med partycentralens slumpgenerator!</p>
+		</div>
+		<div>
+			<button>Snurra fest!</button>
+		</div>
+		<div>
+			<p>Du ska ha en 90-tals fest</p>
+			<p>och servera pizza</p>
+			<p>medans ni lyssnar på hårdrock</p>
+		</div>
+		<div>
+			<form>
+				<h2>Skapa ett eget tema</h2>
+				<input></input>
+			</form>
+		</div>
     </>
   )
 }
